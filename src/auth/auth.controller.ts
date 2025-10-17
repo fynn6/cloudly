590901b4ab8a8f9afe95f162d5a620d41814d341
nihil <fynn_light345@outlook.com>
2025-10-17 23:11:21 +0400

    validation + register + login working

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,22 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { UserService } from '../user/user.service.js';
+import { LoginDto } from './dto/login.do.js';
+import { AuthService } from './auth.service.js';
+import { RegisterDto } from './dto/register.dto.js';
 
 @Controller('auth')
 export class AuthController {
   constructor(
-    private userService: UserService
+    private authService: AuthService
   ) {}
 
   @Post('login')
-  async login(@Body() body) {
-    const user = await this.userService.findUserByEmail(body.email);
+  async login(@Body() loginDto: LoginDto) {
+    const user = await this.authService.login(loginDto);
     return user;
+  }
+
+  @Post('register')
+  async register(@Body() registerDto: RegisterDto) {
+    const user = await this.authService.register(registerDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity.js';
 import { Repository } from 'typeorm';
+import { RegisterDto } from 'src/auth/dto/register.dto.js';
+import bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserService {
@@ -14,5 +16,12 @@ export class UserService {
 
   findUserByEmail(email: string) {
     return this.userRepository.findOneBy({ email: email })
+  }
+
+  async createUser(data: RegisterDto) {
+    const user = new User();
+    user.email = data.email;
+    user.password = await bcrypt.hash(data.password, 10);
+    return this.userRepository.save(user);
   }
 }
